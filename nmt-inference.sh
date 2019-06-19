55d80da20f1619d9df75559f0d53aa0f415667ec
Leonard Punt <leonard@q42.nl>
2019-06-19 13:34:32 +0200

    Small fixes to local inference script

--- nmt-inference.sh
+++ nmt-inference.sh
@@ -2,9 +2,11 @@
 JOB_NAME=$1 # e.g. nmt_20190305_104416
 INPUT_FILE=$2 # e.g. ./my_infer_file.ti
 OUTPUT_FILE=$3 # e.g. /tmp/output_infer
-OUT_DIR="gs://hacking-tigrinya-enti-cloud/$JOB_NAME/nmt_model"
+JOB_DIR="gs://hacking-tigrinya-enti-cloud/$JOB_NAME/ti-en"
 
 python -m nmt.nmt \
-    --out_dir=$OUT_DIR \
+    --out_dir=$JOB_DIR/nmt_model \
     --inference_input_file=$INPUT_FILE \
-    --inference_output_file=$OUTPUT_FILE
+    --inference_output_file=$OUTPUT_FILE \
+    --vocab_prefix=$JOB_DIR/training-data/vocab \
+    --src=ti --tgt=en
